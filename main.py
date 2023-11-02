b54c6b60528f1e07243567c0829757736395a549
Elkarapuzik <shmakov-10@bk.ru>
2023-11-02 19:00:20 +0300

    добавлены функции Try

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import requests
 import os
+from time import sleep
 from termcolor import cprint
 from dotenv import load_dotenv
 from pprint import pprint
@@ -24,45 +25,55 @@ while True:
     print("2.Find the number of clicks on the shortened link")
     print("3.Exit")
 
-    user_choice = int(input("Select the function: "))
-
+    try:
+        user_choice = int(input("Select the function: "))
+    except ValueError:
+        cprint("You have to choose 1/2/3\n", 'red')  
+        continue
 
     if user_choice == 1:
         user_url = input("enter your link-> ")
-        abbreviated_reference = {
+        abbreviated_link = {
             "long_url" : user_url,
         }
 
-        response = requests.post('https://api-ssl.bitly.com/v4/shorten', headers=headers, json=abbreviated_reference)
-        response.raise_for_status()
-
-        
+        try:
+            response = requests.post('https://api-ssl.bitly.com/v4/shorten', headers=headers, json=abbreviated_link)
+            response.raise_for_status()
+        except requests.exceptions.HTTPError:
+            cprint(f"Incorrect link -> {user_url}\n", 'red')
+            continue
         
         cprint(f"""
-Your abbreviated reference ×
-                           │
-                           ╰─> {response.json()['link']}\n""",'green')
+Your abbreviated link ×
+                      │
+                      ╰─> {response.json()['link']}\n""", 'green')
 
-    if user_choice == 2:
-        user_abbreviated_url = input("write your abbreviated reference -> ")
+    elif user_choice == 2:
+        user_abbreviated_url = input("write your abbreviated link -> ")
         abbreviated_params = {
             "unit" : "month",
             "units"  : "-1"
         }
 
-        response = requests.get(f'https://api-ssl.bitly.com/v4/bitlinks/{user_abbreviated_url}/clicks/summary', headers=headers, params=abbreviated_params)
-        response.raise_for_status()
+        try:       
+            response = requests.get(f'https://api-ssl.bitly.com/v4/bitlinks/{user_abbreviated_url}/clicks/summary', headers=headers, params=abbreviated_params)
+            response.raise_for_status()
+        except requests.exceptions.HTTPError:
+            cprint(f"Incorrect shortened link -> {user_abbreviated_url}\n", 'red')
+            continue
 
         cprint(f"""
 click-through rate  ×
                     ╰─> {response.json()['total_clicks']}\n""",'green')
 
-    if user_choice == 3:
-        print("\n")
+    elif user_choice == 3:
+        cprint("You closed the proggram\n", 'magenta')
         exit()
             
 
-    if user_choice > 3:
-        print("""You have to choose 1/2/3/\n""")
+    else:
+        cprint("You have to choose 1/2/3\n", 'red')
+        sleep(1)
 
 
